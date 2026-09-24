c8817fc7a992d5bb15ccdca121013c3b90c6e696
Tanay Bansal <tanaybansal9030@gmail.com>
2026-09-24 10:40:49 +0000

    added infra.yml

--- global/github-oidc/main.tf
+++ global/github-oidc/main.tf
@@ -49,7 +49,7 @@ data "aws_iam_policy_document" "assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repository}:*"]
+      values   = ["repo:TanujBansal1/AWS_Practice_Infra:*"]
     }
   }
 }
